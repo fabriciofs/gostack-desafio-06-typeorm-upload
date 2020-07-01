eff04f89e5140ec460733a57cf44e225727cfa18
Fabrício Ferreira <fabricio@fabriciofs.eti.br>
2020-07-01 07:18:41 -0300

    update cagegory model

--- src/models/Category.ts
+++ src/models/Category.ts
@@ -1,10 +1,22 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+} from 'typeorm';
+
+@Entity('categories')
 class Category {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
+  @CreateDateColumn({ name: 'created_at' })
   created_at: Date;
 
+  @CreateDateColumn({ name: 'updated_at' })
   updated_at: Date;
 }
 

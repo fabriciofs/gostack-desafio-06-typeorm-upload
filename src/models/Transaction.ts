7530f8f73e4f65e57b797df86cb7f56a5e585649
Fabrício Ferreira <fabricio@fabriciofs.eti.br>
2020-07-01 07:18:33 -0300

    update transaction model

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,16 +1,39 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+import Category from './Category';
+
+@Entity('transactions')
 class Transaction {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
+  @Column()
   type: 'income' | 'outcome';
 
+  @Column()
   value: number;
 
+  @Column({ name: 'category_id' })
   category_id: string;
 
+  @ManyToOne(() => Category)
+  @JoinColumn({ name: 'provider_id' })
+  category: Category;
+
+  @CreateDateColumn({ name: 'created_at' })
   created_at: Date;
 
+  @UpdateDateColumn({ name: 'updated_at' })
   updated_at: Date;
 }
 
